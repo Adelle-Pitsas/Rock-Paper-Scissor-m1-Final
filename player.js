4c941cd5264901ae69f35260fe1d707b8966253e
Adelle Pitsas <apitsas4@gmail.com>
2022-09-21 17:30:14 -0400

    create basic structure for player class

---
+++ player.js
@@ -0,0 +1,13 @@
+class Player {
+  constructor(name, token, wins) {
+    this.name = name;
+    this.token = token;
+    this.challenger = []
+    this.wins = wins;
+  }
+
+  takeTurn() {
+    //if its not the current players turn it the other players turn
+    playerChoice = this.challenger
+  }
+}
